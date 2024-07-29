4173ef26c5f653f13c55ec823011291f34a67923
kamjin1996 <kamjin1996@163.com>
2024-07-29 12:02:31 +0800

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import Generator from './generator/page'
 
+
+
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24 dark:bg-gray-900">
